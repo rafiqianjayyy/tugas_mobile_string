de3410a9de533bbdd8cb8f48e2ccb08fb2a444a8
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:18:32 +0700

    Create penggabung.dart

---
+++ penggabung.dart
@@ -0,0 +1,11 @@
+//INI ADALAH PENGGABUNGAN STRING
+ void main(){
+   String namaAwal = 'Muhammad';
+  String namaAKhir = 'Rafiqi';
+
+  var nama1 = namaAwal + ' ' + namaAKhir;
+  var nama2 = 'Muhammad' ' rafiqi' ' anjay';
+
+   print(nama1);
+   print(nama2);
+ }
