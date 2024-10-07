e27db9fc3e70e915d644a23189b603e6d1d2a7e3
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:19:53 +0700

    Create multiline.dart

---
+++ multiline.dart
@@ -0,0 +1,7 @@
+//INI ADALAH MULTILINE STRING
+ void main(){
+   String teksPanjang = ''' aku hari ini akan membuat sebuah paragraf yang panjang beuttt
+   saking panjang nya ya gini udah berapa karakter ini ''';
+
+   print(teksPanjang);
+ }
